752e1cf0808004d3d257a9f3ee6f40f29da99c38
Eric Tran <erictran1547@gmail.com>
2025-03-17 19:39:11 -0700

    Finished Video 33

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from "./Container.jsx";
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
